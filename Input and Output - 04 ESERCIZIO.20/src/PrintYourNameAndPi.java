63494876f748a9e7f292bb08882310127a11e4e3
GiovannaFi <119609101+GiovannaFi@users.noreply.github.com>
2022-12-08 14:34:17 +0100

    Add files via upload

---
+++ Input and Output - 04 ESERCIZIO.20/src/PrintYourNameAndPi.java
@@ -0,0 +1,11 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+
+        String name = "Giovanna";
+        double  numberPi = 3.141590;
+
+        System.out.printf("%s %n", name.toUpperCase());
+        System.out.printf("%f", numberPi);
+
+    }
+}
